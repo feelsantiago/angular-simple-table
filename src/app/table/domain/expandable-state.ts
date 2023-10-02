131aff30f5e4db420d38b28a0470caa38c5437bb
Filipe Santiago <porto.santiago@hotmail.com>
2023-10-02 14:37:46 -0300

    update expandable state

--- src/app/table/domain/expandable-state.ts
+++ src/app/table/domain/expandable-state.ts
@@ -1,25 +1,25 @@
 export type ExpandableRow = number | 'all' | 'none';
 
 export class ExpandableState {
-  constructor(public readonly enabled: boolean, public row: ExpandableRow) {}
+  constructor(public row: ExpandableRow) {}
 
   static init(): ExpandableState {
-    return new ExpandableState(true, 'none');
+    return new ExpandableState('none');
   }
 
   expanded(row: ExpandableRow): boolean {
-    if (this.enabled && this.row === 'all') {
+    if (this.row === 'all') {
       return true;
     }
 
-    return this.enabled && row === this.row;
+    return row === this.row;
   }
 
   expand(row: ExpandableRow): ExpandableState {
     if (this.expanded(row)) {
-      return new ExpandableState(this.enabled, 'none');
+      return new ExpandableState('none');
     }
 
-    return new ExpandableState(this.enabled, row);
+    return new ExpandableState(row);
   }
 }
